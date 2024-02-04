2e65776df62cecf383dbfa341356fd7bc7bbf00d
Marklaurence02 <148670992+Marklaurence02@users.noreply.github.com>
2024-02-04 13:06:02 +0800

    Update Register.php

--- test/Register.php
+++ test/Register.php
@@ -5,13 +5,44 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Change Profile</title>
+    <title>User Registration</title>
 </head>
 <body>
-   
-    </div>
     <div class="container">
         <div class="box form-box">
+
+            <?php
+            include("config.php");
+
+            if(isset($_POST['submit'])){
+                $username = $_POST['username'];
+                $email = $_POST['email'];
+                $address = $_POST['Address'];
+                $school = $_POST['School'];
+                $age = $_POST['Age'];
+                $password = $_POST['password'];
+                $userType = $_POST['userType'];  // Added for user type
+
+                // Verify if email is unique
+                $verify_query = mysqli_query($con, "SELECT Email FROM Users WHERE Email ='$email'");
+
+                if(mysqli_num_rows($verify_query) != 0){
+                    echo "<div class='message'>
+                            <p>This email is already in use. Please choose another one.</p>
+                          </div><br>";
+                    echo "<a href='javascript:self.history.back()'><button class='btn'>Go Back</button>";
+                } else {
+                    // Insert user into the database
+                    mysqli_query($con, "INSERT INTO users (Username, Email, Age, Password, School, Address, UserType) VALUES ('$username', '$email', '$age', '$password','$school','$address', '$userType')") or die("Error Occurred");
+
+                    echo "<div class='message'>
+                            <p>Registration Successful</p>
+                          </div><br>";
+                    echo "<a href='new.php'><button class='btn'>Login Now</button>";
+                }
+            } else {
+            ?>
+
             <header>Sign Up</header>
             <form action="" method="post">
                 <div class="field input">
@@ -26,7 +57,25 @@
 
                 <div class="field input">
                     <label for="age">Age</label>
-                    <input type="Age" name="Age" id="Age" required>
+                    <input type="number" name="Age" id="Age" required>
+                </div>
+
+                <div class="field input">
+                    <label for="School">School</label>
+                    <input type="text" name="School" id="school" required>
+                </div>
+
+                <div class="field input">
+                    <label for="Address">Address</label>
+                    <input type="text" name="Address" id="address" required>
+                </div>
+
+                <div class="field input">
+                    <label for="userType">User Type</label>
+                    <select name="userType" id="userType" required>
+                        <option value="student">Student</option>
+                        <option value="teacher">Teacher</option>
+                    </select>
                 </div>
 
                 <div class="field input">
@@ -35,13 +84,15 @@
                 </div>
 
                 <div class="field">
-                    <input type="submit"class="btn" name="submit" value="Login">
+                    <input type="submit" class="btn" name="submit" value="Register">
                 </div>
 
                 <div class="link">
-                   Already member? <a href="new.php">Sign in</a>
+                   Already a member? <a href="new.php">Sign in</a>
                 </div>
             </form>
+            <?php } ?>
+
         </div>
     </div>
 </body>
